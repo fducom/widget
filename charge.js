d93d36034ffa02b9febd8cad4929d165c119566b
Dima <fducom@bigmir.net>
2023-09-15 18:23:42 +0300

    Update charge.js

--- charge.js
+++ charge.js
@@ -1,21 +1,40 @@
-// Змінні, що відображатимуть інформацію про зарядку
+// Функція для відправки API-запиту
+async function sendApiRequest() {
+  try {
+    const apiEndpoint = "https://example.com/api/stopCharging"; // Замініть на власний API-адресу
+    const response = await fetch(apiEndpoint, {
+      method: "POST", // або "GET" в залежності від потреби
+      // Додайте інші параметри запиту, якщо необхідно
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Помилка при відправці API-запиту:", error);
+    return null;
+  }
+}
+
+// Створення віджета
+const widget = new ListWidget();
+widget.backgroundColor = new Color("#3498db"); // Колір фону віджета
+
+// Відправка API-запиту при натисканні на кнопку
+const button = widget.addText("Зупинити зарядку");
+button.textColor = new Color("#ffffff"); // Колір тексту кнопки
+button.centerAlignText(); // Вирівнювання тексту по центру
+
+button.url = "scriptable:///run?name=StopCharging"; // Замініть на ім'я вашого скрипту в Scriptable
+
+// Відображення інформації про зарядку (приклад)
 const chargingStatus = "Зарядка: Активна";
 const batteryLevel = "Рівень батареї: 80%";
 const chargingSpeed = "Швидкість зарядки: 10 кВт";
 
-// Створюємо віджет
-const widget = new ListWidget();
-
-// Додаємо інформацію про зарядку
 widget.addText(chargingStatus);
 widget.addText(batteryLevel);
 widget.addText(chargingSpeed);
 
-// Додаємо кнопку "Стоп"
-const stopButton = widget.addButton("Стоп");
-stopButton.url = "your_api_endpoint_to_stop_charging"; // Замініть це на власний API-запит для зупинки зарядки
-
-// Відображаємо віджет
+// Відображення віджета
 if (config.runsInWidget) {
   Script.setWidget(widget);
 } else {
